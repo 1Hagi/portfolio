d5b738038349526932f6909cd494e00087021336
Diego N. Cabrera <87397817+1Hagi@users.noreply.github.com>
2025-12-14 03:03:28 -0300

    Maquetado y Estilos de los Tags.

---
+++ js/scroll-infinito.js
@@ -0,0 +1,38 @@
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+// Duplicate each item in the scroller for infinite effect
+scrollers.forEach(scroller => {
+const inner = scroller.querySelector(".scroller__inner");
+const items = Array.from(inner.children);
+
+items.forEach(item => {
+    const clone = item.cloneNode(true);
+    clone.setAttribute("aria-hidden", "true");
+    inner.appendChild(clone);
+});
+});
